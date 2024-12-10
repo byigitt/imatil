a822a26dadb28af96c6576cca2eeefe5c0ed2ffa
byigitt <137426514+byigitt@users.noreply.github.com>
2024-12-10 21:37:43 +0300

    feat: add autoprefixer and update Tailwind CSS configuration
    - Added autoprefixer as a dependency to enhance CSS compatibility across different browsers.
    - Updated postcss.config.mjs to include autoprefixer in the PostCSS plugins.
    - Modified tailwind.config.ts to streamline content paths for better file matching.
    - Adjusted globals.css to refine body styling with font feature settings for improved typography.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,13 +3,13 @@ import type { Config } from "tailwindcss";
 const config = {
     darkMode: ["class"],
     content: [
-    "./src/pages/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
-    "./src/app/**/*.{ts,tsx}",
-    "./src/src/**/*.{ts,tsx}",
-  ],
-  prefix: "",
-  theme: {
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    prefix: "",
+    theme: {
   	container: {
   		center: true,
   		padding: "2rem",
